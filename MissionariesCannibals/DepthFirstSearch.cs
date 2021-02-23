f9aeb860e83ee0c0c0a147e69456077962c1fa59
Arturich <artursavchuk7@gmail.com>
2021-02-23 18:26:49 +0200

    limited depth search added

---
+++ MissionariesCannibals/DepthFirstSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissionariesCannibals
+{
+    //depth limited search
+    class DepthFirstSearch
+    {
+        public State exec(State initialState)
+        {
+
+            //init depth limit
+            int limit = 20;
+            return recursiveDLS(initialState, limit);
+        }
+
+        private State recursiveDLS(State state, int limit)
+        {
+            if (state.isGoal())
+            {
+                return state;
+            }
+            else if (limit == 0)
+            {
+                return null;
+            }
+            else
+            {
+                List<State> successors = state.generateSuccessors();
+                foreach (State child in successors)
+                {
+                    State result = recursiveDLS(child, limit - 1);
+                    if (null != result)
+                    {
+                        return result;
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
